5739ef9978b95882c6858165513a27fb5159964d
VyProductions <76977808+VyProductions@users.noreply.github.com>
2022-06-03 06:57:24 -0700

    Update WSL-Setup.ps1
    Changes to end of script.

--- WSL-Setup.ps1
+++ WSL-Setup.ps1
@@ -11,12 +11,4 @@ echo '[Setup] Enabling Windows Virtual Machine Platform and WSL Features on this
 Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All
 
-echo "[System] A restart is recommended to complete this setup."
-$ans = Read-Host "Would you like to restart now? (Y/N)"
-
-if ($ans -eq "Y" -OR $ans -eq "y") {
-    echo "[Setup] Restarting..."
-    Restart-Computer
-} elseif ($ans -eq "N" -OR $ans -eq "n") {
-    echo "[Setup] Please restart your computer when convenient to complete this setup."
-}
+echo "[Setup] Done."
